30baebf91d99f27266f6534aa03b36fe3c0f5c45
Matt Pestridge <mpestridge1986@gmail.com>
2019-10-15 22:21:25 -0600

    added a new algorithm solving a time based problem

--- practice.js
+++ practice.js
@@ -40,7 +40,8 @@ function staircase(n) {
   }
 }
 staircase(6);
-
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
+//Tallest Birthday Candles
 // Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
 
 // For example, arr = [1, 3, 5, 7, 9]. Our minimum sum is 1 + 3 + 5 + 7 = 16 and our maximum sum is 3 + 5 + 7 + 9 = 24. We would print:
@@ -82,3 +83,48 @@ function miniMaxSum(arr) {
   console.log(minSum + ' ' + maxSum);
 }
 miniMaxSum();
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
+//Time Conversion
+// Given a time in 12-hour AM/PM format, convert it to military (24-hour) time.
+
+// Note: Midnight is 12:00:00AM on a 12-hour clock, and 00:00:00 on a 24-hour clock. Noon is 12:00:00PM on a 12-hour clock, and 12:00:00 on a 24-hour clock.
+
+// Function Description:
+// Complete the timeConversion function in the editor below. It should return a new string representing the input time in 24 hour format.
+
+// timeConversion has the following parameter(s):
+// s: a string representing time in 12 hour format
+
+// Input Format:
+// A single string s containing a time in 12-hour clock format (i.e.: hh:mm:ss:AM or hh:mm:ss:PM), where 01<=hh<=12 and 00<=mm,ss<=59.
+
+// Constraints:
+// All input times are valid
+
+// Output Format:
+// Convert and print the given time in 24-hour format, where 00<=hh<=23.
+
+// Sample Input:
+// 07:05:45PM
+
+// Sample Output:
+// 19:05:45
+
+//My code:
+function timeConversion(s) {
+  if (s[8] === 'P') {
+    let updatedHour = (Number(s.substring(0, 2))) + 12;
+    if (updatedHour >= 24) {
+      s = s.substring(0, 8);
+    } else {
+      s = String(updatedHour) + s.substring(2, 8);
+    }
+  } else {
+    if (Number(s.substring(0, 2)) === 12) {
+      s = '00' + s.substring(2, 8);
+    } else {
+      s = s.substring(0, 8);
+    }
+  }
+  return s;
+}
